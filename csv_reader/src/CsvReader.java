64310608cf62f0f7d880d090ea7987b80bc65761
mrbeonghia <ntnghia199x@gmail.com>
2020-12-01 10:46:10 +0700

    csv-file

--- csv_reader/src/CsvReader.java
+++ csv_reader/src/CsvReader.java
@@ -4,49 +4,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CsvReader {
-    private static final String COMMA = ",";
-
     public static void main(String[] args) {
-        BufferedReader br = null;
+        String line = "";
+        String splitBy = ",";
         try {
-            String line;
-            br = new BufferedReader(new FileReader("D:/IntelliJ/CSV_file/contries.csv"));
-
-            // How to read file in java line by line?
-            while ((line = br.readLine()) != null) {
-                printContry(parseCsvLine(line));
+            BufferedReader br = new BufferedReader(new FileReader("D:/IntelliJ/CSV_file/contries.csv"));
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                String[] country = line.split(splitBy);    // use comma as separator
+                System.out.println("Country [id = " + country[0] + ", code = " + country[1] + ", name = " + country[2] + "]");
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (Exception exception){
-            exception.printStackTrace();
-        }
-        finally {
-            try {
-                if (br != null)
-                    br.close();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        }
-    }
-    public static List<String> parseCsvLine(String csvLine) {
-        List<String> result = new ArrayList<String>();
-        if (csvLine != null) {
-            String[] splitData = csvLine.split(COMMA);
-            for (int i = 0; i < splitData.length; i++) {
-                result.add(splitData[i]);
-            }
-        }
-        return result;
-    }
-
-    public static void printContry(List<String> country) {
-        System.out.println(
-                "Country [id = "
-                        + country.get(0)
-                        + ", code = " + country.get(1)
-                        + " , name = " + country.get(2)
-                        + "]");
     }
 
 
